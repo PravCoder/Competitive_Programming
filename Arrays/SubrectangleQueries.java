20b762432eac5236bdf57334e40c54c6c08e89ff
pravboi <pravachanpatra@gmail.com>
2023-09-04 11:15:16 -0500

    1476. Subrectangle Queries

---
+++ Arrays/SubrectangleQueries.java
@@ -0,0 +1,25 @@
+
+
+public class SubrectangleQueries {
+    
+    private int[][] rectangle;
+    private int rows;
+    private int cols;
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+        this.rows = this.rectangle.length;
+        this.cols = this.rectangle[0].length;
+    }
+    
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i=row1; i<=row2; i++) {
+            for (int j=col1; j<=col2; j++) {
+                this.rectangle[i][j] = newValue;
+            }
+        }
+    }
+    
+    public int getValue(int row, int col) {
+        return this.rectangle[row][col];
+    }
+}
