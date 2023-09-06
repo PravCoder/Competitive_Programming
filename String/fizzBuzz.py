a3924daf53ab575aecdde1b75885c7a52fa08962
pravboi <pravachanpatra@gmail.com>
2023-09-06 06:01:53 -0500

    412. Fizz Buzz

---
+++ String/fizzBuzz.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fizzBuzz(self, n: int):
+
+        output = []
+
+        for i in range(1, n+1):
+            if i%3==0 and i%5 == 0:
+                output.append("FizzBuzz")
+            elif i%3 == 0 and i%5 != 0:
+                output.append("Fizz")
+            elif i%5 == 0 and i%3 != 0:
+                output.append("Buzz")
+            elif i%3 != 0 and i%5 != 0:
+                output.append(str(i))
+
+        return output
+
+# NOTE: array is 1-indexed
