34ca6d7f4d046673e082fafe702891719ef25be7
pravboi <pravachanpatra@gmail.com>
2023-09-21 11:37:28 -0500

    1763. Longest Nice Substring

---
+++ String/LongestNiceSubString.java
@@ -0,0 +1,37 @@
+
+class LongestNiceSubString {
+    public String longestNiceSubstring(String s) {
+
+        int maxLength = 0;
+        String maxSub = "";
+
+        for (int i=0; i<s.length(); i++) {
+            String curSub = ""+s.charAt(i);
+            for (int j=i+1; j<s.length(); j++) {
+                curSub += s.charAt(j);
+                if (this.isNice(curSub) == true) {
+                    System.out.println(curSub + " " +maxLength);
+                    if (curSub.length() > maxLength) {
+                        maxLength = curSub.length();
+                        maxSub = curSub;
+                    }
+                }
+            }
+        }
+        return maxSub;
+        
+    }
+
+    public boolean isNice(String s) {
+        for (int i=0; i<s.length(); i++) {
+            char cur = s.charAt(i);
+            if (Character.isUpperCase(cur) == true && s.contains(String.valueOf(Character.toLowerCase(cur))) ==false) {
+                return false;
+            }
+            if (Character.isLowerCase(cur)==true && s.contains(String.valueOf(Character.toUpperCase(cur))) ==false) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
