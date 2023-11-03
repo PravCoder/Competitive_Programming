aab30d09e1280f54c07a6dd1ea8f2234626f2e98
pravboi <pravachanpatra@gmail.com>
2023-11-03 07:18:17 -0500

    2125. Number of Laser Beams in a Bank

--- Arrays/NumberOfLaserBeamsInBank.py
+++ Arrays/NumberOfLaserBeamsInBank.py
@@ -19,3 +19,36 @@ class Solution:
 
 # NOTE: there is a laserbeam between 2 devices if they are on different rows, 
 # and the rows between them are empty. We removed empty rows so we dont have to check for r1 < i < r2 ith row being empty. 
+# 2125. Number of Laser Beams in a Bank
+
+# class Solution {
+#     public int numberOfBeams(String[] bank) {
+
+#         ArrayList<String> new_bank = new ArrayList<String>();
+
+#         for (String row: bank){
+#             if (row.indexOf('1') != -1) {
+#                 new_bank.add(row);
+#             }
+#         }
+#         int total = 0;
+
+#         for (int i=1; i<new_bank.size(); i++) {
+#             String prev_row = new_bank.get(i-1);
+#             String cur_row = new_bank.get(i);
+#             total += getLaserCount(prev_row) * getLaserCount(cur_row);
+#         }
+#         return total;
+        
+#     }
+
+#     public int getLaserCount(String row) {
+#         int count = 0;
+#         for (int i=0; i<row.length(); i++) {
+#             if (row.charAt(i) == '1') {
+#                 count += 1;
+#             }
+#         }
+#         return count;
+#     }
+# }
