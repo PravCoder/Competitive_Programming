d0ab41321ddbcde112bef46d91f203ecb926f881
pravboi <pravachanpatra@gmail.com>
2024-10-05 13:30:05 -0500

    NAQ problem C

---
+++ NAQ/A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+/* 
+
+Problem:
+-Each card has value
+-Add card: increase total score by cards value
+-Multiply card: multiply score by cards value
+process cards left to right, compute score for a row of cards.
+
+
+Input:
+n=number of cards, k=maximum number of multiply cards you can use.
+next n lines contain either a or m and an int v.
+a=add cards
+m=multiply cards
+v=value of card
+
+Output:
+for each n output lines output max score you can get for a subseq
+of length at most 1,2,3,...,n.
+*/
+
+
+
+int main() {
+    string input;
+    cin >> input;
+    
+    std::cout<<"Hello World";
+
+    return 0;
+}
---
+++ NAQ/C.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+/* 
+n=number of candidate problems
+*/
+
+
+
+int main() {
+    int n;
+    cin >> n;
+        
+    int num_excluded = 0;
+    for (int i=0; i<n; i++) {
+        int cur_prob_rating;
+        cin >> cur_prob_rating;
+        
+        if (cur_prob_rating %2 != 0) {
+            num_excluded += 1;
+        }
+        
+    }
+    
+    cout << num_excluded;
+    return 0;
+}
