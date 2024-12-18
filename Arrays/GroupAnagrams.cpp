88c095b093678a307a715cb83673331736340f2a
Pravachan Patra <pravachanpatra@gmail.com>
2024-12-18 16:55:40 -0600

    49. Group Anagrams

--- Arrays/GroupAnagrams.cpp
+++ Arrays/GroupAnagrams.cpp
@@ -76,3 +76,36 @@ public:
         return true;
     }
 };
+
+
+// PASSED
+class Solution {
+public:
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // each element is a group, in each group is that set of anagrams
+        vector<vector<string>> output;
+        // map where key is the base-sorted-anagram-string, and value is the group of all anagrams in strs
+        unordered_map<string, vector<string>> anagram_groups;
+        
+        // iterate each string
+        for (int i=0; i<strs.size(); i++) {
+            string cur_str = strs[i];  
+            string cur_sorted_str = strs[i];  // stores the sorted-base-key-string for map
+            // sort the cur-string to use as a key in anagram-groups
+            sort(cur_sorted_str.begin(), cur_sorted_str.end());  
+            // if the sorted-base-string key doesnt exist in map, create a new key-val-pair and add cur-str to it. 
+            // if the sorted-base-string key does exist in map, add the original string to that keys group
+            anagram_groups[cur_sorted_str].push_back(cur_str); 
+        }
+
+        // iterate each key-value or base-string anagram-group pairs
+        for (auto& pair : anagram_groups) {
+            // get the value of each anagram-pair in map, and add it to result
+            output.push_back(pair.second);
+        }
+
+        return output;
+    }
+
+};
